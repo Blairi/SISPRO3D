f990d27657a2bd2ff5a28bf6fef1a009edf1a23c
blairi <axel.avi.fer@gmail.com>
2026-05-05 20:22:55 -0600

    feat: add enums

---
+++ src/main/java/mx/unam/dgtic/domain/OrderStatus.java
@@ -0,0 +1,5 @@
+package mx.unam.dgtic.domain;
+
+public enum OrderStatus {
+    PENDING, IN_PROGRESS, IN_REVIEW, COMPLETED, CANCELLED
+}
---
+++ src/main/java/mx/unam/dgtic/domain/QuoteStatus.java
@@ -0,0 +1,5 @@
+package mx.unam.dgtic.domain;
+
+public enum QuoteStatus {
+    PENDING, ACCEPTED, REJECTED, EXPIRED
+}
