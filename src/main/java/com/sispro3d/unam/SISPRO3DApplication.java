5cb5322d2d43fe4fadd483e07bb626cc81833d98
Axel Montiel <axel.avi.fer@gmail.com>
2026-07-02 19:39:57 -0600

    feat: implement spring boot

---
+++ src/main/java/com/sispro3d/unam/SISPRO3DApplication.java
@@ -0,0 +1,10 @@
+package com.sispro3d.unam;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SISPRO3DApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SISPRO3DApplication.class, args);
+    }
+}
---
+++ src/main/java/com/sispro3d/unam/SISPRO3DApplicationRunner.java
@@ -0,0 +1,51 @@
+package com.sispro3d.unam;
+
+import com.sispro3d.unam.controller.AccountController;
+import com.sispro3d.unam.domain.UserType;
+import com.sispro3d.unam.dto.AccountDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Configuration
+public class SISPRO3DApplicationRunner implements CommandLineRunner {
+
+    @Autowired
+    private AccountController accountController;
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("===== SISPRO3D & Spring Boot =====");
+
+        accountController.displayAllAccounts();
+
+        AccountDTO newAccount = new AccountDTO(
+                0, "Axel",
+                "Nuevo", "axel@unam.mx",
+                "+52 55321233", "dummy@password",
+                UserType.ADMIN, LocalDateTime.now()
+        );
+        accountController.createAccount(newAccount);
+        accountController.displayAllAccounts();
+
+        // Editar algún registro
+        List<AccountDTO> accounts = accountController.getAllAccounts();
+        AccountDTO accountToEdit = accounts.getLast();
+        System.out.println("Editando una cuenta accountToEdit = " + accountToEdit);
+        accountToEdit.setName("NOBRE NUEVO EDITADO");
+        accountController.updateAccount(accountToEdit.getIdUser(), accountToEdit);
+
+        // Listar registros para verificar la edición
+        accountController.displayAllAccounts();
+
+        // Eliminar algún registro
+        System.out.println("Eliminando accountToEdit = " + accountToEdit);
+        accountController.deleteAccount(accountToEdit.getIdUser());
+
+        // Listar registros para verificar la eliminación
+        accountController.displayAllAccounts();
+    }
+}
