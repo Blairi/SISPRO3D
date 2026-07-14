06f3eed9235d09a6ee5d345b2c7654c2fb21c867
Axel Montiel <axel.avi.fer@gmail.com>
2026-07-13 19:10:46 -0600

    feat: define phone size

--- src/main/java/com/sispro3d/unam/user/domain/Account.java
+++ src/main/java/com/sispro3d/unam/user/domain/Account.java
@@ -28,6 +28,7 @@ public class Account {
     @Column(nullable = false, unique = true)
     private String email;
 
+    @Column(length = 20)
     private String phone;
 
     @Column(nullable = false)
