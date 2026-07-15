85a529ba07a6be6e2fea856a50f1cf47fbba74a1
Axel Montiel <axel.avi.fer@gmail.com>
2026-07-14 18:55:54 -0600

    feat: implement entity and repository for category

--- src/main/java/com/sispro3d/unam/category/domain/Category.java
+++ src/main/java/com/sispro3d/unam/category/domain/Category.java
@@ -1,14 +1,25 @@
 package com.sispro3d.unam.category.domain;
 
+import jakarta.persistence.*;
 import lombok.*;
 
+@Entity
+@Table(name = "category")
 @Getter
 @Setter
 @ToString
 @NoArgsConstructor
 @AllArgsConstructor
 public class Category {
-    private int id;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_category")
+    private Long id;
+
+    @Column(nullable = false, unique = true)
     private String name;
+
+    @Column(columnDefinition = "TEXT")
     private String description;
 }
--- src/main/java/com/sispro3d/unam/category/repository/CategoryRepository.java
+++ src/main/java/com/sispro3d/unam/category/repository/CategoryRepository.java
@@ -1,5 +1,9 @@
 package com.sispro3d.unam.category.repository;
 
+import com.sispro3d.unam.category.domain.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface CategoryRepository {
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
 }
