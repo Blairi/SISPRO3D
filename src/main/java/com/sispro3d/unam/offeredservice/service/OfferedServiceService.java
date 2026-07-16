58889e4e57a1f13ea2cd289776005ef06537b5d3
Axel Montiel <axel.avi.fer@gmail.com>
2026-07-16 17:12:29 -0600

    feat: implement approve and reject service

--- src/main/java/com/sispro3d/unam/offeredservice/service/OfferedServiceService.java
+++ src/main/java/com/sispro3d/unam/offeredservice/service/OfferedServiceService.java
@@ -5,4 +5,6 @@ import com.sispro3d.unam.offeredservice.dto.OfferedServiceRequest;
 import com.sispro3d.unam.offeredservice.dto.OfferedServiceResponse;
 
 public interface OfferedServiceService extends CrudService<OfferedServiceRequest, OfferedServiceResponse, Long> {
+    OfferedServiceResponse approve(Long id, Long adminId);
+    OfferedServiceResponse reject(Long id, Long adminId);
 }
